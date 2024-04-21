7cee497e9b99387bd940a0ec85051f06d92bc09b
Deepanshu <yadavdeepanshu000@gmail.com>
2024-04-21 19:12:39 +0530

    ToDo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  //const items = ["item1", "item2", "item3", "item4", "item5"];
+  const [items, setItems] = useState([
+    "item1", 
+    "item2", 
+    "item3",
+    "item4",
+    "item5",]);
+
+  
+
+  const [value, setValue] = useState("");
+  function onValueChange(e){
+    console.log(e.target.value);
+    setValue(e.target.value);
+  }
+
+  function onAdd(){
+    console.log(value);
+
+    const newItems = [...items,  value];
+
+    setItems(newItems);
+    setValue("");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ToDo List</h1>
+        <input type="text" placeholder="Enter todo item" 
+        value={value}
+        onChange={onValueChange}
+        />
+        <button onClick={onAdd}>Add</button>
+
+        <ol>
+          {items.map((item, i) => (
+            <li
+            key={i}
+              style={{ 
+                color: "green",
+             }}>
+              {item}</li>
+          ))}
+        </ol>
       </header>
     </div>
   );
